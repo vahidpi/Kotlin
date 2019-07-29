94bc901b803171da05f148427d8b9e28b531585f
Vahid Piroozbakht <vahid.piroozbakht@paf.com>
2019-07-29 14:35:07 +0300

    swoosh pass data extra file

--- Swoosh/app/src/main/java/com/example/swoosh/SkillActivity.kt
+++ Swoosh/app/src/main/java/com/example/swoosh/SkillActivity.kt
@@ -1,12 +1,18 @@
 package com.example.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+
+    var league=""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        league=intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 }
